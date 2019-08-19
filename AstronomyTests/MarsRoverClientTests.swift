b1cee1b928a1f049267ebf920fd7f8e2aefa7a6c
seanacres <6416690+seanacres@users.noreply.github.com>
2019-08-19 18:07:57 -0400

    Add empty tests to complete

---
+++ AstronomyTests/MarsRoverClientTests.swift
@@ -0,0 +1,21 @@
+//
+//  MarsRoverClientTests.swift
+//  AstronomyTests
+//
+//  Created by Sean Acres on 8/19/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class MarsRoverClientTests: XCTestCase {
+
+    func testFetchingRover() {
+        
+    }
+    
+    func testFetchingRoverPhotos() {
+        
+    }
+
+}
