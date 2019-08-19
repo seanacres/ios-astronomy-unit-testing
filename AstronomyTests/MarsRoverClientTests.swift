3094bdd3332b7071268c4c5f6e0a5c53155ae239
seanacres <6416690+seanacres@users.noreply.github.com>
2019-08-19 18:28:48 -0400

    Complete tests for fetching rover and photos from mock json data

--- AstronomyTests/MarsRoverClientTests.swift
+++ AstronomyTests/MarsRoverClientTests.swift
@@ -12,10 +12,35 @@ import XCTest
 class MarsRoverClientTests: XCTestCase {
 
     func testFetchingRover() {
+        var mockLoader = MockLoader()
+        mockLoader.data = validRoverJSON
         
+        let marsRoverClient = MarsRoverClient(dataLoader: mockLoader)
+        let fetchRoverExpectation = expectation(description: "waiting for rover results")
+        
+        marsRoverClient.fetchMarsRover(named: "Curiosity") { (rover, error) in
+            fetchRoverExpectation.fulfill()
+            XCTAssertNotNil(rover)
+        }
+        
+        waitForExpectations(timeout: 10)
     }
     
     func testFetchingRoverPhotos() {
+        var mockLoader = MockLoader()
+        mockLoader.data = validSol1JSON
+        let solDescription = SolDescription(sol: 1, totalPhotos: 10, cameras: [""])
+        let rover = MarsRover(name: "Curiousity", launchDate: Date(), landingDate: Date(), status: .active, maxSol: 10, maxDate: Date(), numberOfPhotos: 10, solDescriptions: [solDescription])
+        
+        let marsRoverClient = MarsRoverClient(dataLoader: mockLoader)
+        let fetchPhotosExpectation = expectation(description: "waiting for rover photos")
+        
+        marsRoverClient.fetchPhotos(from: rover, onSol: 1) { (photos, error) in
+            fetchPhotosExpectation.fulfill()
+            XCTAssertTrue(photos!.count > 1)
+        }
+        
+        waitForExpectations(timeout: 10)
         
     }
 
